80e230f30c28dd01fb3a5f86ffbae0900da6f5a5
FL <felip3.fl@gmail.com>
2024-05-23 22:13:01 -0300

    New main program

---
+++ EasyD/EasyD/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyD
+{
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            Ytdlp ytdlp = new Ytdlp();
+            var foi = ytdlp.Download();
+        }
+    }
+
+}
