1758167626b5d693bd7592ca95f753866e02c8ce
FL <felip3.fl@gmail.com>
2024-08-01 21:51:45 -0300

    New models

---
+++ Source/Core/Model/MessageStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Model
+{
+    public class MessageStatus
+    {
+        public List<string> Info { get; set; }
+        public List<string> Error { get; set; }
+    }
+}
---
+++ Source/Core/Model/Video.cs
@@ -0,0 +1,9 @@
+namespace Core.Model
+{
+    public class Video
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string RealizeDate { get; set; }
+    }
+}
