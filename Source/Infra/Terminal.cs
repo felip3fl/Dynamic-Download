68c78b92b4da48a2144f1e7e05100cf1ae0c6f01
Felip3FL <felip3.fl@gmail.com>
2025-01-22 13:13:56 -0300

    Impoved terminal method

--- Source/Infra/Terminal.cs
+++ Source/Infra/Terminal.cs
@@ -10,35 +10,47 @@ namespace Infra
 
         public bool Download(string videoAddress)
         {
-            Console.Clear();
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
-            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            var process = new Process();
+            var startInfo = new ProcessStartInfo();
 
-            //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.FileName         = executableAddress;
             startInfo.WorkingDirectory = @"D:\Videos\Youtube Temp\";
             startInfo.Arguments        = parameters + " " + videoAddress;
             process.StartInfo          = startInfo;
+
             process.Start();
 
             return true;
         }
 
+        private void test(Process process)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+            Console.ResetColor();
+        }
+
+        public void ShowConsoleParameters()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{executableAddress} {parameters} \n");
+            Console.ResetColor();
+        }
 
         public string DownloadWithConsole(string videoAddress)
         {
-            var process                = new Process();
-            var messages               = new StringBuilder();
-            ProcessStartInfo startInfo = new ProcessStartInfo();
+            var process   = new Process();
+            var messages  = new StringBuilder();
+            var startInfo = new ProcessStartInfo();
 
             if (videoAddress == "")
             {
-                videoAddress = "https://www.youtube.com/watch?v=0TZ1krFdzoQ";    
+                videoAddress = "https://www.youtube.com/watch?v=2Ylc0_g3AKU";    
             }
 
             startInfo.FileName               = executableAddress;
             startInfo.WorkingDirectory       = @"D:\Videos\Youtube Temp\";
-            startInfo.Arguments              = parameters + videoAddress;
+            startInfo.Arguments              = parameters + " " + videoAddress;
             startInfo.RedirectStandardError  = true;
             startInfo.RedirectStandardOutput = true;
             process.StartInfo                = startInfo;
