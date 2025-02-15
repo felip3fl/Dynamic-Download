c1c02b4b2cd0d036ffbd19596214366abff5decc
Felip3FL <felip3.fl@gmail.com>
2025-02-15 13:28:59 -0300

    Update project

--- Source/Core/DownloadService.cs
+++ Source/Core/DownloadService.cs
@@ -1,25 +1,46 @@
 ﻿using Core.Model;
 using File;
 using Infra;
+using Newtonsoft.Json;
+using System.Formats.Asn1;
 using System.Text.RegularExpressions;
 
 namespace Core
 {
     public class DownloadService
     {
-        Terminal terminal = new Terminal();
+        private Terminal terminal = new();
+        private List<Video> VideoList = new();
 
-        public string Download(string url)
+        public bool Download(string url)
         {
             var video = new Video();
 
-            video.Url = RemoveAdditionalParameters(url); 
+            LoadVideo();
 
-            var consoleMessage = terminal.Download(video.Url);
-            //var listOfMessages = ConvertMessageToList(consoleMessage);
-            //video.RealizeDate = CheckReturn("");
+            video.Url = RemoveAdditionalParameters(url);
 
-            return consoleMessage.ToString();
+            if (CheckIfExistVideo(video.Url))
+                return false;
+
+            terminal.ShowConsoleParameters();
+
+            var consoleMessage = terminal.CheckVideoDetails(url);
+
+            if (!string.IsNullOrEmpty(consoleMessage))
+            {
+                var listOfMessages = ConvertMessageToList(consoleMessage);
+                ShowErroConsole(listOfMessages);
+                video.RealizeDate = CheckReturn(listOfMessages);
+                SalveVideo();
+                return false;
+            }
+
+
+            terminal.Download(url);
+            
+
+            return true;
         }
 
         public List<string> ConvertMessageToList(string messages)
@@ -28,21 +49,54 @@ namespace Core
             return listOfMessages;
         }
 
-        private DateTime CheckReturn(string message)
+        private void SalveVideo()
+        {
+            JsonFile jsonFile = new JsonFile();
+            jsonFile.Save(VideoList);
+        }
+
+        private bool CheckIfExistVideo(string url)
+        {
+            var search = VideoList.Where(f => f.Url == url).FirstOrDefault();
+            if (String.IsNullOrEmpty(search.Url))
+                return false;
+
+            return true;
+        }
+
+        private void LoadVideo()
+        {
+            JsonFile jsonFile = new JsonFile();
+            var fileContent = jsonFile.Load();
+            var listVideo = fileContent.ConvertStringToVideo();
+            VideoList = listVideo;
+        }
+
+        private void ShowErroConsole(List<string> listOfmessage)
+        {
+            foreach (var item in listOfmessage)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(item);
+                Console.ResetColor();
+            }
+        }
+
+        private DateTime CheckReturn(List<string> listOfmessage)
         {
             //message = "This live event will begin in 11 days";
             //message = "ERROR: [youtube] 2Ylc0_g3AKU: Premieres in 52 minutes"
 
-            if (!message.Contains("Premieres in"))
+            var myRegex = new Regex(@"\d+ (minute|day|mounth|hour|second)");
+            var resultList = listOfmessage.Where(f => myRegex.IsMatch(f)).ToList().FirstOrDefault();
+
+            if (string.IsNullOrEmpty(resultList))
                 return default;
 
-            //if (!message.Contains("This live event will"))
-            //    return default;
-
             var futureDate = DateTime.Now;
-            var daysRemaining = Regex.Match(message, @"\d+ (minute|day|mounth|hour|second)").Value;
-
+            var daysRemaining = Regex.Match(resultList, @"\d+ (minute|day|mounth|hour|second)").Value;
             var values = daysRemaining.Split(" ");
+
             switch (values[1])
             {
                 case "minute":
@@ -63,7 +117,7 @@ namespace Core
 
             //var dateRealize = dateNow.AddDays(1);
 
-            return futureDate.AddHours(2);
+            return futureDate.AddHours(1);
         }
 
         private string RemoveAdditionalParameters(string url)
@@ -76,6 +130,15 @@ namespace Core
                 return url;
 
             return urlWithNoParameters;
+        }
+
+
+    }
+
+    public static class ObjectExtension
+    {
+        public static List<Video> ConvertStringToVideo(this string obj) {
+            return JsonConvert.DeserializeObject<List<Video>>(obj);
         }
     }
 }
--- Source/File/JsonFile.cs
+++ Source/File/JsonFile.cs
@@ -1,17 +1,22 @@
-﻿namespace File
+﻿using Newtonsoft.Json;
+using System.Text.Json.Nodes;
+
+namespace File
 {
     public class JsonFile
     {
-        
-        public void Save(string path){
+        private const string defaultPath = "\\";
+        private const string fileName = "Videos.json";
 
-            System.IO.File.WriteAllText(path, "");
+        public void Save(string path, string content){
+
+            System.IO.File.WriteAllText(path, content);
         } 
 
-        public string[] LoadAllLines(string path)
+        public string[] ReadAllLines(string path)
         {
             //check if file exists
-            if (!System.IO.File.Exists(path))
+            if (checkIfFileExists(path))
             {
                 throw new System.IO.FileNotFoundException();
             }
@@ -19,6 +24,55 @@
             var fileContent = System.IO.File.ReadAllLines(path);
 
             return fileContent;
+        }
+
+        public string ReadAllText(string path)
+        {
+            //check if file exists
+
+            //var filesWithAddress = Directory.GetFiles(defaultPath);
+
+            if (checkIfFileExists(path))
+            {
+                throw new System.IO.FileNotFoundException();
+            }
+
+            var fileContent = System.IO.File.ReadAllText(fileName);
+
+            return fileContent;
+        }
+
+        private bool checkIfFileExists(string path)
+        {
+            return System.IO.File.Exists(path);
+        }
+
+        public void Save(object content)
+        {
+            var seriazedObject = JsonConvert.SerializeObject(content, Formatting.Indented);
+            
+            System.IO.File.WriteAllText(defaultPath + fileName, seriazedObject);
+        }
+
+        public string Load()
+        {
+
+
+
+            var jsonObject = new JsonObject()
+            {
+                ["descricao"] = "Plataforma .NET",
+                ["anoInicial"] = 2002,
+                ["pessoasChave"] = new JsonArray("Anders Hejlsberg", "Scott Hanselman", "Mads Torgersen", "Miguel de Icaza"),
+                ["ferramentas"] = new JsonObject
+                {
+                    ["microsoft"] = new JsonArray("Visual Studio 2019", "Visual Studio 2022", "Visual Studio Code"),
+                    ["jetBrains"] = "Rider"
+                }
+            };
+
+
+            return ReadAllText(defaultPath + fileName);
         }
 
     }
